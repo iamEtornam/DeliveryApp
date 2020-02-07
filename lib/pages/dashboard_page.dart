1f26dd2f9cbb687de2cd044286903384a798951c
Etornam Sunu <sunumacbright@gmail.com>
2020-02-07 15:25:40 +0000

    Update dashboard_page.dart

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,4 @@
-
+import 'package:delivery_app/pages/services_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
@@ -9,6 +9,12 @@ class DashboardPage extends StatefulWidget {
 
 class _DashboardPageState extends State<DashboardPage> {
   TextEditingController _controller = TextEditingController();
+  List bottomItems = [
+    [AntDesign.home, 'Home', false],
+    [MaterialCommunityIcons.truck_delivery, 'Tracking', false],
+    [Icons.history, 'History', false],
+    [Ionicons.ios_person, 'Profile', false]
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +47,9 @@ class _DashboardPageState extends State<DashboardPage> {
                                       color: Colors.white,
                                       fontWeight: FontWeight.bold),
                                 ),
-                                SizedBox(height: 8,),
+                                SizedBox(
+                                  height: 8,
+                                ),
                                 Text('what will you do today?',
                                     style: TextStyle(
                                         fontSize: 14, color: Colors.white))
@@ -52,6 +60,8 @@ class _DashboardPageState extends State<DashboardPage> {
                               width: 50,
                               decoration: BoxDecoration(
                                   color: Colors.indigoAccent,
+                                  image: DecorationImage(image: 
+                                  ExactAssetImage('assets/avarta.jpg'),fit: BoxFit.contain),
                                   border:
                                       Border.all(color: Colors.white, width: 1),
                                   borderRadius: BorderRadius.circular(45)),
@@ -100,7 +110,7 @@ class _DashboardPageState extends State<DashboardPage> {
                       elevation: 4,
                       borderRadius: BorderRadius.circular(8),
                       child: Padding(
-                        padding: const EdgeInsets.fromLTRB(16.0,10,16,10),
+                        padding: const EdgeInsets.fromLTRB(16.0, 10, 16, 10),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
@@ -198,44 +208,81 @@ class _DashboardPageState extends State<DashboardPage> {
             margin: EdgeInsets.all(16),
             padding: const EdgeInsets.all(8),
             decoration: BoxDecoration(
-                color: Colors.indigoAccent, borderRadius: BorderRadius.circular(8)),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.monetization_on,size: 60,color: Colors.white,),
-                    SizedBox(width:15),
-                    Expanded(
-                                          child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Congratulations!',style: TextStyle(color:Colors.white, fontSize: 18,fontWeight: FontWeight.bold),),
-                          SizedBox(height:8),
-                          Text('''Lorem ipsum dolor sit amet, consectetur adipiscing elit.''',style: TextStyle(color:Colors.white60, fontSize: 14))
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-          ),
-          SizedBox(height:10),
-          Padding(
-            padding: const EdgeInsets.only(left:16.0,right: 16.0),
+                color: Colors.indigoAccent,
+                borderRadius: BorderRadius.circular(8)),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('My Services',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                Text('View All',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14,color: Colors.indigoAccent))
-
+                Material(
+                           borderRadius: BorderRadius.circular(45),
+              color: Colors.white.withOpacity(.2),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/happy.png',height: 40,width: 40,),
+                  )),
+                SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Congratulations!',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                          '''Lorem ipsum dolor sit amet, consectetur adipiscing elit.''',
+                          style: TextStyle(color: Colors.white60, fontSize: 14))
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-          SizedBox(height:10),
-           buildServicesContainer(context: context,voidCallback: null,title: 'Courier',subTitle: '10k courier',icon: Icons.monetization_on),
-               buildServicesContainer(context: context,voidCallback: null,title: 'Shipping',subTitle: 'save delivery',icon: Icons.monetization_on),
-           SizedBox(height:10),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('My Services',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ServicesPage()),
+                    );
+                  },
+                  child: Text('View All',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.indigoAccent)),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          buildServicesContainer(
+              context: context,
+              voidCallback: null,
+              title: 'Courier',
+              subTitle: '10k courier',
+              icon: 'assets/courier.png'),
+          buildServicesContainer(
+              context: context,
+              voidCallback: null,
+              title: 'Shipping',
+              subTitle: 'save delivery',
+              icon: 'assets/package.png'),
+          SizedBox(height: 10),
         ],
       ),
       bottomNavigationBar: Material(
@@ -247,18 +294,49 @@ class _DashboardPageState extends State<DashboardPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[Icon(AntDesign.home), Text('Home')],
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    bottomItems[0][2] = true;
+                    bottomItems[1][2] = false;
+                    bottomItems[2][2] = false;
+                    bottomItems[3][2] = false;
+                  });
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(AntDesign.home,
+                        color: bottomItems[0][2] ? Colors.black : Colors.grey),
+                    Text('Home',
+                        style: TextStyle(
+                            color:
+                                bottomItems[0][2] ? Colors.black : Colors.grey))
+                  ],
+                ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(MaterialCommunityIcons.truck_delivery),
-                  Text('Tracking')
-                ],
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    bottomItems[0][2] = false;
+                    bottomItems[1][2] = true;
+                    bottomItems[2][2] = false;
+                    bottomItems[3][2] = false;
+                  });
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(MaterialCommunityIcons.truck_delivery,
+                        color: bottomItems[1][2] ? Colors.black : Colors.grey),
+                    Text('Tracking',
+                        style: TextStyle(
+                            color:
+                                bottomItems[1][2] ? Colors.black : Colors.grey))
+                  ],
+                ),
               ),
               Container(
                 width: 50,
@@ -272,15 +350,53 @@ class _DashboardPageState extends State<DashboardPage> {
                   color: Colors.white,
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[Icon(Icons.history), Text('History')],
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    bottomItems[0][2] = false;
+                    bottomItems[1][2] = false;
+                    bottomItems[2][2] = true;
+                    bottomItems[3][2] = false;
+                  });
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.history,
+                      color: bottomItems[2][2] ? Colors.black : Colors.grey,
+                    ),
+                    Text(
+                      'History',
+                      style: TextStyle(
+                          color:
+                              bottomItems[2][2] ? Colors.black : Colors.grey),
+                    )
+                  ],
+                ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[Icon(Ionicons.ios_person), Text('Profile')],
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    bottomItems[0][2] = false;
+                    bottomItems[1][2] = false;
+                    bottomItems[2][2] = false;
+                    bottomItems[3][2] = true;
+                  });
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Ionicons.ios_person,
+                        color: bottomItems[3][2] ? Colors.black : Colors.grey),
+                    Text('Profile',
+                        style: TextStyle(
+                            color:
+                                bottomItems[3][2] ? Colors.black : Colors.grey))
+                  ],
+                ),
               ),
             ],
           ),
@@ -289,35 +405,57 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-   buildServicesContainer({BuildContext context, IconData icon, String title, String subTitle, VoidCallback voidCallback})  {
-    return Container(
-          height: 70,
-          width: MediaQuery.of(context).size.width,
-          margin: EdgeInsets.fromLTRB(16,5,16,6),
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-              color: Colors.grey.shade300, borderRadius: BorderRadius.circular(8)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+  buildServicesContainer(
+      {BuildContext context,
+      String icon,
+      String title,
+      String subTitle,
+      VoidCallback voidCallback}) {
+    return Theme(
+      data: ThemeData(
+          brightness: Brightness.light, applyElevationOverlayColor: true),
+      child: Container(
+        height: 70,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.fromLTRB(16, 5, 16, 6),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade200.withOpacity(.5),
+            borderRadius: BorderRadius.circular(8)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Material(
+              borderRadius: BorderRadius.circular(45),
+              color: Colors.indigoAccent.withOpacity(.3),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(icon),
+              )),
+            SizedBox(width: 15),
+            Expanded(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Icon(icon,size: 50,color: Colors.indigoAccent,),
-                  SizedBox(width:15),
-                  Expanded(
-                                        child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('$title',style: TextStyle( fontSize: 18,fontWeight: FontWeight.bold),),
-                        SizedBox(height:5),
-                        Text('$subTitle',style: TextStyle(color:Colors.pink, fontSize: 14))
-                      ],
-                    ),
+                  Text(
+                    '$title',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  IconButton(icon: Icon(Icons.arrow_forward_ios,color:Colors.grey.shade400), onPressed: voidCallback)
+                  SizedBox(height: 5),
+                  Text('$subTitle',
+                      style: TextStyle(color: Colors.pink, fontSize: 14))
                 ],
               ),
-        );
+            ),
+            IconButton(
+                icon:
+                    Icon(Icons.arrow_forward_ios, color: Colors.grey.shade400),
+                onPressed: voidCallback)
+          ],
+        ),
+      ),
+    );
   }
 }
