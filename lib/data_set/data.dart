5fc8f03a81420081e47660893503297a48dd0b3e
Etornam Sunu <sunumacbright@gmail.com>
2020-02-07 15:25:35 +0000

    Create data.dart

---
+++ lib/data_set/data.dart
@@ -0,0 +1,8 @@
+List services = [
+  [null, 'Courier', '10k courier', 'assets/courier.png'],
+  [null, 'Shipping', 'save delivery', 'assets/package.png'],
+  [null, 'Fast Delivery', '2 days - 6 days', 'assets/delivery.png'],
+  [null, 'The Warranty', 'if damage returned', 'assets/warranty.png'],
+  [null, 'Affordable Prices', 'very economical', 'assets/affordable.png'],
+  [null, 'Recommendation', 'nice and comfortable', 'assets/recommend.png']
+];
