efd8fc51d54a3e7f667c8bfd550d32eded216c77
Etornam Sunu <sunumacbright@gmail.com>
2020-02-07 15:25:37 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
+import 'dart:io';
+
+import 'package:delivery_app/blocs/change_layout_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/dashboard_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Deliver',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.lato().fontFamily,
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ChangeLayoutBloc>.value(
+            value: ChangeLayoutBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Deliver',
+        theme: ThemeData(
+          brightness: Brightness.light,
+            fontFamily: GoogleFonts.lato().fontFamily,
+            primarySwatch: Colors.indigo,
+            appBarTheme: AppBarTheme(color: Colors.indigo,elevation: Platform.isIOS ? 0 : 4)),
+        home: DashboardPage(),
       ),
-      home: DashboardPage(),
     );
   }
 }
